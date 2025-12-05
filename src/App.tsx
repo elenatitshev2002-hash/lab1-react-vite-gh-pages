75d91bab850cacfa87e806f021065969e56f3af0
Владимир Шевченко <elena.titshev2002@gmail.com>
2025-12-05 23:38:58 +0300

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Counter from './components/Counter'
 import UserTable from './components/UserTable'
-
+import Counter from './components/Counter'
 import './App.css'
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src="/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Counter /> {/* ← Теперь здесь только компонент */}
+      <Counter />
       <UserTable />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
